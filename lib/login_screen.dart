8353c97e281874a02379c251bfae081991dd724b
Seonyoung Pang <sunyeongbang98@gmail.com>
2024-12-15 20:14:41 +0900

    update main login screen ui

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,135 +1,143 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive_login_ui/pallets.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_responsive_login_ui/widgets/loginin_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.white12,
+        width: 2,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    );
+
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: 500,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Sign in.'),
-              const SizedBox(
-                height: 15,
-              ),
-              // InkWell(
-              //   onTap: () {},
-              //   child: Container(
-              //     decoration: BoxDecoration(
-              //       border: Border.all(
-              //         width: 2.0,
-              //         color: Colors.white24,
-              //       ),
-              //       borderRadius: BorderRadius.circular(16.0),
-              //     ),
-              //     child: Row(
-              //       children: [
-              //         SvgPicture.asset(
-              //           'assets/svgs/g_logo.svg',
-              //           width: 20,
-              //           colorFilter: const ColorFilter.mode(
-              //             Pallete.whiteColor,
-              //             BlendMode.srcIn,
-              //           ),
-              //         ),
-              //         const Text('Continue with Google'),
-              //       ],
-              //     ),
-              //   ),
-              // ),
-              //google
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4.0),
+      body: Column(
+        children: [
+          Image.asset('assets/images/signin_balls.png'),
+          Center(
+            child: SizedBox(
+              width: 500,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Sign in.',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    side: const BorderSide(width: 2, color: Colors.white12),
-                  ),
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 30,
-                      vertical: 20,
+                    const SizedBox(
+                      height: 40,
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset(
-                          'assets/svgs/g_logo.svg',
-                          width: 20,
-                          colorFilter: const ColorFilter.mode(
-                            Pallete.whiteColor,
-                            BlendMode.srcIn,
+                    // InkWell(
+                    //   onTap: () {},
+                    //   child: Container(
+                    //     decoration: BoxDecoration(
+                    //       border: Border.all(
+                    //         width: 2.0,
+                    //         color: Colors.white24,
+                    //       ),
+                    //       borderRadius: BorderRadius.circular(16.0),
+                    //     ),
+                    //     child: Row(
+                    //       children: [
+                    //         SvgPicture.asset(
+                    //           'assets/svgs/g_logo.svg',
+                    //           width: 20,
+                    //           colorFilter: const ColorFilter.mode(
+                    //             Pallete.whiteColor,
+                    //             BlendMode.srcIn,
+                    //           ),
+                    //         ),
+                    //         const Text('Continue with Google'),
+                    //       ],
+                    //     ),
+                    //   ),
+                    // ),
+                    //google
+                    const LoginButton(loginCompany: 'Google'),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    //facebook button
+                    const LoginButton(loginCompany: 'Facebook'),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text('or'),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Email',
+                          hintStyle: TextStyle(color: Colors.white54),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 25,
+                            horizontal: 20,
+                          ),
+                          border: border,
+                          focusedBorder: border,
+                          enabledBorder: border),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: TextStyle(color: Colors.white54),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 25,
+                            horizontal: 20,
+                          ),
+                          border: border,
+                          focusedBorder: border,
+                          enabledBorder: border),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: const LinearGradient(
+                            colors: <Color>[
+                              Pallete.gradient1,
+                              Pallete.gradient2,
+                              Pallete.gradient3,
+                            ],
                           ),
                         ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        const Text(
-                          'Continue with Google',
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: const Text(
+                          'Sign in',
                           style: TextStyle(
                             color: Pallete.whiteColor,
-                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
                           ),
+                          textAlign: TextAlign.center,
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ),
-              //facebook button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4.0),
-                    ),
-                    side: const BorderSide(width: 2, color: Colors.white12),
-                  ),
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 30,
-                      vertical: 20,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset(
-                          'assets/svgs/f_logo.svg',
-                          width: 20,
-                          colorFilter: const ColorFilter.mode(
-                            Pallete.whiteColor,
-                            BlendMode.srcIn,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        const Text(
-                          'Continue with Facebook',
-                          style: TextStyle(
-                            color: Pallete.whiteColor,
-                            fontSize: 16.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
